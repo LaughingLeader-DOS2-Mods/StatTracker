b430f8d3637ff22c9abac00eec11109a38ceb703
LaughingLeader <laughingleader@gmail.com>
2021-06-29 23:12:49 -0500

    Text tweak, setup coloring for the path of blood stats

--- Mods/StatTracker_0ea6834c-f042-4fa4-918c-71637b98ab4a/Story/RawFiles/Lua/Client/TooltipHandler.lua
+++ Mods/StatTracker_0ea6834c-f042-4fa4-918c-71637b98ab4a/Story/RawFiles/Lua/Client/TooltipHandler.lua
@@ -9,7 +9,7 @@ local PathOfBloodRelatedStats = {
 ---@param stat CustomStatData
 ---@param tooltip TooltipData
 local function OnCustomStatTooltip(character, stat, tooltip)
-	if OriginsCampaignEnabled and PathOfBloodRelatedStats[stat.ID] then
+	if PathOfBloodRelatedStats[stat.ID] then
 		if character:HasTag("LLSTAT_PathOfBloodFailed") then
 			tooltip:AppendElement({
 				Type="StatsTalentsMalus",
@@ -24,4 +24,24 @@ local function OnCustomStatTooltip(character, stat, tooltip)
 	end
 end
 
-Game.Tooltip.RegisterListener("CustomStat", nil, OnCustomStatTooltip)
+local stats = {
+	ID.Murders,
+	ID.Kills,
+	ID.SoulsEaten,
+	ID.StolenItems,
+}
+
+local registeredListeners = false
+
+Ext.RegisterListener("SessionLoaded", function()
+	if Ext.IsModLoaded("1301db3d-1f54-4e98-9be5-5094030916e4") and not registeredListeners then
+		CustomStatSystem:RegisterStatAddedHandler(stats, function(id, stat, character, stat_mc)
+			if not character:HasTag("LLSTAT_PathOfBloodFailed") then
+				stat_mc.label_txt.htmlText = string.format("<font color='#0C3D10'>%s</font>", stat_mc.label_txt.htmlText)
+				--stat_mc.label_txt.htmlText = string.format("<font color='#7a1221'>%s</font>", stat_mc.label_txt.htmlText)
+			end
+		end)
+		Game.Tooltip.RegisterListener("CustomStat", nil, OnCustomStatTooltip)
+		registeredListeners = true
+	end
+end)
