ec3da448f187384eb020c1663bb95fc4bf583666
LaughingLeader <laughingleader@gmail.com>
2021-06-29 23:51:05 -0500

    Text update

--- Mods/StatTracker_0ea6834c-f042-4fa4-918c-71637b98ab4a/Story/RawFiles/Lua/Shared/_InitShared.lua
+++ Mods/StatTracker_0ea6834c-f042-4fa4-918c-71637b98ab4a/Story/RawFiles/Lua/Shared/_InitShared.lua
@@ -10,6 +10,8 @@ ID = {
 	SoulsEaten = "LLSTATS_SoulsEaten",
 }
 
+--Mods.LeaderLib.CustomStatSystem.Stats["0ea6834c-f042-4fa4-918c-71637b98ab4a"].LLSTATS_TotalDamage:SetValue(host.MyGuid, 9999999999999999)
+--print(Mods.LeaderLib.CustomStatSystem.Stats["0ea6834c-f042-4fa4-918c-71637b98ab4a"].LLSTATS_TotalDamage:GetValue(host.MyGuid))
 OriginsCampaignEnabled = false
 
 Ext.Require("Shared/PointListeners.lua")
