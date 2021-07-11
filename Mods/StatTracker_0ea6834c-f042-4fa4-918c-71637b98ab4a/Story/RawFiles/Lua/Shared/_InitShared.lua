55955a003301b59fc65b7191d085ccc4ef75dd83
LaughingLeader <laughingleader@gmail.com>
2021-07-10 23:53:46 -0500

    Commenting out tests

--- Mods/StatTracker_0ea6834c-f042-4fa4-918c-71637b98ab4a/Story/RawFiles/Lua/Shared/_InitShared.lua
+++ Mods/StatTracker_0ea6834c-f042-4fa4-918c-71637b98ab4a/Story/RawFiles/Lua/Shared/_InitShared.lua
@@ -11,7 +11,9 @@ ID = {
 }
 
 --Mods.LeaderLib.CustomStatSystem.Stats["0ea6834c-f042-4fa4-918c-71637b98ab4a"].LLSTATS_TotalDamage:SetValue(host.MyGuid, 9999999999999999)
+--Mods.LeaderLib.CustomStatSystem.Stats["0ea6834c-f042-4fa4-918c-71637b98ab4a"].LLSTATS_TotalDamage:SetValue(host.MyGuid, math.maxinteger)
 --print(Mods.LeaderLib.CustomStatSystem.Stats["0ea6834c-f042-4fa4-918c-71637b98ab4a"].LLSTATS_TotalDamage:GetValue(host.MyGuid))
+--local player = Ext.GetCharacter(host.MyGuid); player:SetCustomStat("e2d8e73d-eb9b-4088-a938-3b832abda912", 9999999999999999); print(player:GetCustomStat("e2d8e73d-eb9b-4088-a938-3b832abda912"));
 OriginsCampaignEnabled = false
 
 Ext.Require("Shared/PointListeners.lua")
